2b1741db020f89caf2756271082f44c029a090a8
MohiAssaf <mohamedabuassaf@gmail.com>
2025-07-09 01:03:50 +0300

    add the global css for the nativewind to work

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import "./global.css";
 import "./localization/index";
 import ChatScreen from "./components/ChatScreen";
 import { SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView className="flex-1">
       <ChatScreen />
     </SafeAreaView>
   );
